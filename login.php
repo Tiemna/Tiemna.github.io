5d0c3aa21c37b5097ba3e4bb95e31c098131ce50
Tiemna <144110284+Tiemna@users.noreply.github.com>
2025-04-16 10:08:12 +0200

    phpfull

--- login.php
+++ login.php
@@ -16,3 +16,24 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     }
 }
 ?>
+
+<!DOCTYPE html>
+<html lang="cs">
+<head>
+    <meta charset="UTF-8">
+    <title>Přihlášení</title>
+</head>
+<body>
+    <h2>Přihlášení</h2>
+
+    <?php if (!empty($error)): ?>
+        <p style="color:red;"><?php echo htmlspecialchars($error); ?></p>
+    <?php endif; ?>
+
+    <form method="POST" action="login.php?<?php echo htmlspecialchars($_SERVER['QUERY_STRING']); ?>">
+        <label>Uživatel: <input type="text" name="username"></label><br><br>
+        <label>Heslo: <input type="password" name="password"></label><br><br>
+        <input type="submit" value="Přihlásit se">
+    </form>
+</body>
+</html>
